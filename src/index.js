9f1321d60bd13dca053b0d2f34692c2698293f63
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-10 17:52:19 +0100

    header tablet + desktop responsiveness

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+import pfp from "./assets/images/pfp.png";
+import github from "./assets/icons/github.svg";
+import linkedin from "./assets/icons/linkedin.svg";
+import x from "./assets/icons/x.svg";
