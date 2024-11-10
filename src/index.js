c3dca115ae91cb49d69e85773e2af073cf68e0fb
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-10 20:09:57 +0100

    style: main grid

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import pfp from "./assets/images/pfp.png";
 import github from "./assets/icons/github.svg";
 import linkedin from "./assets/icons/linkedin.svg";
 import x from "./assets/icons/x.svg";
+import externalLink from "./assets/icons/external-link.svg";
+import placeholder from "./assets/images/placeholder.png";
