758e0880137d1d4c9631d83189f5ead8409c6720
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-12 17:04:30 +0100

    fix: fixed widths on imgs, optimized imgs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles.css";
-import pfp from "./assets/images/pfp.png";
+import pfp from "./assets/images/pfp.jpg";
 import github from "./assets/icons/github.svg";
 import linkedin from "./assets/icons/linkedin.svg";
 import x from "./assets/icons/x.svg";
 import externalLink from "./assets/icons/external-link.svg";
 import placeholder from "./assets/images/placeholder.png";
-import pic2 from "./assets/images/pic2.png";
+import pic2 from "./assets/images/pic2.jpg";
 import phoneIcon from "./assets/icons/phone.svg";
 import emailIcon from "./assets/icons/mail.svg";
